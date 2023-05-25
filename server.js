5b274e24cd2cc65a4e75d7294e0798e84b01ca0d
Juan-P1224 <juanpa122412@gmail.com>
2023-05-25 18:17:11 -0500

    Api nueva

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require('express')
+const mysql = require('mysql')
+const myconn = require('express-myconnection')
+
+const routes = require('./ApiAccesorios/routes')
+const routes2 = require('./ApiDisplay/routes2')
+const routesVenta = require('./ApiVentas/routesVenta')
+const cors = require('cors')
+
+const app = express()
+
+
+app.set('port', process.env.PORT || 9001)
+
+const dbOptions = {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'juan',
+    database: 'juanmcell'
+}
+
+
+// middlewares -------------------------------------
+app.use(myconn(mysql, dbOptions, 'single'))
+app.use(express.json())
+app.use(cors())
+// routes -------------------------------------------
+app.get('/', (req, res)=>{
+    res.send('Welcome to my API')
+})
+app.use('/api', routes);
+app.use('/display', routes2);
+app.use('/venta', routesVenta);
+
+
+
+// server running -----------------------------------
+app.listen(app.get('port'), ()=>{
+    console.log('server running on port', app.get('port'))
+})
